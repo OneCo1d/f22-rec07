b90f983884fe3832fad67c3dc5f2c4777bc2aea4
Onecold <wyh19990727@gmail.com>
2023-10-10 23:32:46 -0400

    Implement undo

--- back-end/src/main/java/game/Game.java
+++ back-end/src/main/java/game/Game.java
@@ -68,4 +68,12 @@ public class Game {
             return board.getCell(1, 1);
         return null;
     }
+
+    public Game undo() {
+        if (!history.isEmpty()) {
+            return history.get(history.size() - 1);
+        }
+        return this;
+    }
+    
 }
